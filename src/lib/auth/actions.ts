ed020623792700ca6be77c9e949f5965d055e109
kunalPisolkar24 <kpisolkar24@gmail.com>
2026-06-29 20:58:58 +0530

    remove neon auth server, client, actions and api route

--- src/lib/auth/actions.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { getAuth } from "@/lib/auth/server";
-import { redirect } from "next/navigation";
-
-export async function signOutAction() {
-  await getAuth().signOut();
-  redirect("/");
-}
--- src/lib/auth/client.ts
+++
@@ -1,5 +0,0 @@
-import { createAuthClient } from "@neondatabase/auth";
-
-export const authClient = createAuthClient(
-  process.env.NEXT_PUBLIC_NEON_AUTH_BASE_URL!,
-);
